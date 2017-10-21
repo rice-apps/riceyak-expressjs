be2c57b4c3517c71f34aca7e169da7a43b986818
Cade Ritter <ritter.cade@gmail.com>
2017-10-21 04:55:32 -0500

    replace_me db uri

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     secret: 'TEST_SECRET',
-    db_uri: 'mongodb://localhost/RiceYak',
+    db_uri: 'REPLACE_ME',
     CASValidateURL: 'https://idp.rice.edu/idp/profile/cas/serviceValidate',
     thisServiceURL: 'http://localhost:4200/auth',
     frontendURL: 'http://localhost:4200'
