084c2c8a1adcdf6c2c16639ce02fd334d6be6eda
Cade Ritter <ritter.cade@gmail.com>
2017-10-20 20:06:44 -0500

    update cors

--- src/app.js
+++ src/app.js
@@ -14,11 +14,7 @@ var app = express();
 app.use(morgan('combined'));
 
 /* Enable CORS */
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", config.frontendURL);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors());
 
 /* Declare our routes */
 app.use('/posts', postController);
