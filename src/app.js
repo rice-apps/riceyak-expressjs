7d07ac44e8daaefe92973b91383df9ec007bfefd
Cade Ritter <ritter.cade@gmail.com>
2017-11-28 14:51:19 -0600

    add helmet

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var RateLimit = require('express-rate-limit');
+var helmet = require('helmet');
 
 var db = require('./db');
 var postController = require('./controllers/post-controller');
@@ -25,12 +26,15 @@ app.use(bodyParser.json());
 app.use('/api/posts', postController);
 app.use('/api/auth', authController);
 
+/* Use helmet to set various HTTP headers for security */
+app.use(helmet());
+
 /* Set up global rate limiting */
 app.enable('trust proxy');
-var apiLimiter = new RateLimit({
-    windowMs: 15*60*1000, // 15 min
-    max: 100
-});
-app.use('/api', apiLimiter);
+// var apiLimiter = new RateLimit({
+//     windowMs: 15*60*1000, // 15 min
+//     max: 100
+// });
+// app.use('/api', apiLimiter);
 
 module.exports = app;
