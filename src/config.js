833dc9f6141d191f1d9e5edacdbfa046f4431fa0
root <root@riceapps.org>
2018-04-07 20:11:29 -0500

    fix config and package

--- src/config.js
+++ src/config.js
@@ -4,8 +4,8 @@ if (process.env.NODE_ENV === 'prod') {
     salt: 'TEST_SALT',
     db_uri: 'mongodb://riceucsclub:r1ce4pps$wag1@ds121945.mlab.com:21945/riceyak-dev',
     CASValidateURL: 'https://idp.rice.edu/idp/profile/cas/serviceValidate',
-    thisServiceURL: 'http://speak.riceapps.org/auth',
-    frontendURL: 'http://speak.riceapps.org'
+    thisServiceURL: 'https://speak.riceapps.org/auth',
+    frontendURL: 'https://speak.riceapps.org'
   };
 } else {
   module.exports = {
